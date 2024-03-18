0e9ec53439ba6bfd1d821bcfe9e8ec6d97423645
B1ckbeard <46893211+B1ckbeard@users.noreply.github.com>
2024-03-18 20:24:32 +0300

    message fix

--- frontend/src/components/Chat/Message.jsx
+++ frontend/src/components/Chat/Message.jsx
@@ -6,6 +6,7 @@ const Message = ({ message }) => {
     <div className="text-break mb-2">
       <b>{message.username}</b>
       :
+      {' '}
       {leoProfanity.clean(message.body)}
     </div>
   );
